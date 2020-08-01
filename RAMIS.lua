1f32ecd0928525352bc8b85794a777b3700c717d
TEAMRAMIS <68572461+TEAMRAMIS@users.noreply.github.com>
2020-08-01 18:35:27 +0300

    Update RAMIS.lua

--- RAMIS.lua
+++ RAMIS.lua
@@ -161,7 +161,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/ZNZNN/Files_Ramis/master/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/TEAMRAMIS/Files_Ramis/master/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -982,7 +982,7 @@ end
 
 if text == 'تحديث السورس ★' and SudoBot(msg) then 
 os.execute('rm -rf RAMIS.lua')
-os.execute('wget https://raw.githubusercontent.com/ZNZNN/RAMIS/master/RAMIS.lua')
+os.execute('wget https://raw.githubusercontent.com/TEAMRAMIS/RAMIS/master/RAMIS.lua')
 send(msg.chat_id_, msg.id_,' ★∫ تم تحديث السورس \n ★∫ لديك اخر اصدار لسورس راميس\n ★∫ الاصدار » { 1.3v}')
 dofile('RAMIS.lua')  
 end
@@ -2229,7 +2229,7 @@ end
 return false
 end
 os.execute('rm -rf RAMIS.lua')
-os.execute('wget https://raw.githubusercontent.com/ZNZNN/RAMIS/master/RAMIS.lua')
+os.execute('wget https://raw.githubusercontent.com/TEAMRAMIS/RAMIS/master/RAMIS.lua')
 send(msg.chat_id_, msg.id_,' ★∫ تم تحديث السورس \n ★∫ لديك اخر اصدار لسورس راميس\n ★∫ الاصدار » { 1.3v}')
 dofile('RAMIS.lua')  
 end
@@ -3379,7 +3379,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if SudoBot(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/ZNZNN/Files_Ramis/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/TEAMRAMIS/Files_Ramis/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3417,7 +3417,7 @@ t = " ★∫ الملف » "..file.."\n ★∫ تم تعطيل ملف \n"
 else
 t = " ★∫ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/ZNZNN/Files_Ramis/master/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/TEAMRAMIS/Files_Ramis/master/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3437,7 +3437,7 @@ t = " ★∫ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ★∫ الملف » "..file.."\n ★∫ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/ZNZNN/Files_Ramis/master/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/TEAMRAMIS/Files_Ramis/master/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
